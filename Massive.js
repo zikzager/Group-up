5a1ae455c29b6716c2ea428bf3101a4634360792
zikzager <60246677+zikzager@users.noreply.github.com>
2020-01-31 18:44:31 +0700

    Add files via upload

---
+++ Massive.js
@@ -0,0 +1,36 @@
+'use strict';
+let arr = [];
+for (let i = 20; i > 0; i -= 1) {
+    arr.push(Math.floor(Math.random()*100));
+}
+console.log(arr);//генерация массива
+
+function noobSort(arrToSort){
+    let arrSorted = [];
+    let arrToSort1= Array.from(arrToSort);
+    for (let i = arrToSort1.length; i > 0; i -= 1){
+        let x=arrToSort1.pop();
+        for (let j = 0; j < arrToSort1.length; j += 1) {
+            if (x > arrToSort1[j]) {
+                let a = arrToSort1[j];
+                arrToSort1[j] = x;
+                x = a;
+            }
+        }
+    arrSorted.push(x);
+    }
+    return arrSorted;
+}
+console.log(noobSort(arr))
+function isArraySorted(sorted, original){
+    let check;
+    for (let i=0; i < sorted.length; i+=1){
+        if (sorted[i]>sorted[i+1]){
+            check = false;
+            break;
+        }
+        else check = true;
+    }
+    return check;
+}
+console.log(isArraySorted(noobSort(arr), arr))
